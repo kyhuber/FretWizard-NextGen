17ea0f3db6b490d02365873e790a839b23da4b01
Kyle Huber <kyhuber@gmail.com>
2023-11-17 22:03:06 -0800

    Add initial Svelte components

---
+++ fretwizard-svelte/components/app.svelte
@@ -0,0 +1,16 @@
+<!-- App.svelte -->
+<script>
+    import { Router, Link } from 'svelte-routing';
+    import InstrumentSetup from './InstrumentSetup.svelte';
+    import ScaleVisualizer from './ScaleVisualizer.svelte';
+</script>
+
+<nav>
+    <Link to="/">Instrument Setup</Link>
+    <Link to="/scale-visualizer">Scale Visualizer</Link>
+</nav>
+
+<Router>
+    <InstrumentSetup path="/" />
+    <ScaleVisualizer path="/scale-visualizer/:numberOfStrings/:tuning" />
+</Router>
--- fretwizard-svelte/src/App.svelte
+++ fretwizard-svelte/src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+	import InstrumentSetup from '../components/InstrumentSetup.svelte';
+  </script>
+  
+  <main>
+	<InstrumentSetup />
+  </main>
+  
 
 <style>
 	main {
@@ -15,12 +15,6 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
