927ebcf6a6aec561d89cae46de2b6341b87994de
Kyle Huber <kyhuber@gmail.com>
2023-11-19 23:48:51 -0800

    modularize JS

---
+++ fretwizard-svelte/components/fretboard.js
@@ -0,0 +1,129 @@
+// Import the static data
+import staticData from '../static_data.json';
+
+// Define constants
+const NUM_FRETS = 24; // Maximum number of frets for any instrument
+const NUM_STRINGS = 6; // Maximum number of strings for any instrument
+
+// Initialize variables for user input
+let selectedInstrument = '';
+let selectedTuning = '';
+let selectedKey = '';
+let selectedScaleType = '';
+
+// Initialize variables for fretboard data
+let fretboard = [];
+
+// Function to populate the fretboard
+function populateFretboard() {
+  // Clear the existing fretboard data
+  fretboard = [];
+
+  // Check if all required input is provided
+  if (!selectedInstrument || !selectedTuning || !selectedKey || !selectedScaleType) {
+    return;
+  }
+
+  // Get the instrument data
+  const instrumentData = staticData.instrumentStrings[selectedInstrument];
+  const numStrings = instrumentData.length;
+
+  // Get the tuning data
+  const tuningOptions = staticData.tuningOptions[selectedInstrument];
+  const selectedTuningData = tuningOptions.find(option => option.name === selectedTuning);
+
+  // Get the scale intervals for the selected scale type
+  const scaleIntervals = staticData.scaleIntervals[selectedScaleType];
+
+  // Iterate through the frets and strings to populate the fretboard
+  for (let stringIndex = 0; stringIndex < numStrings; stringIndex++) {
+    const openNote = selectedTuningData.notes[stringIndex];
+    const stringNotes = [openNote];
+
+    for (let fretIndex = 1; fretIndex <= NUM_FRETS; fretIndex++) {
+      const prevNote = stringNotes[fretIndex - 1];
+      const interval = scaleIntervals[(fretIndex - 1) % scaleIntervals.length];
+      const nextNoteIndex = (staticData.allNotes.indexOf(prevNote) + interval) % 12;
+      const nextNote = staticData.allNotes[nextNoteIndex];
+      stringNotes.push(nextNote);
+    }
+
+    fretboard.push(stringNotes);
+  }
+}
+
+// Function to calculate the positions of dots on the fretboard based on the selected key and scale
+function calculateDots(key, scale) {
+    // Define the intervals for the selected scale
+    const scaleIntervals = staticData.scaleIntervals[scale];
+
+    // Initialize an array to store dot positions
+    const dotPositions = [];
+
+    // Initialize variables to keep track of the current note and fret
+    let currentNote = key;
+    let currentFret = 0;
+
+    // Loop through the scale intervals to calculate dot positions
+    for (const interval of scaleIntervals) {
+        // Calculate the next fret based on the interval
+        currentFret += interval;
+
+        // Calculate the string and note for the current position
+        const stringIndex = findStringForNote(currentNote);
+        const noteAtPosition = mapNoteAtPosition(stringIndex, currentFret);
+
+        // If the note is found, add it to the dot positions
+        if (noteAtPosition !== "undefined") {
+            dotPositions.push({ string: stringIndex, fret: currentFret });
+        }
+
+        // Update the current note based on the interval
+        currentNote = getNextNoteInScale(currentNote, interval);
+    }
+
+    return dotPositions;
+}
+
+// Function to find the string index that corresponds to a note
+function findStringForNote(note) {
+    if (selectedInstrument in tuningOptions) {
+        const notes = tuningOptions[selectedInstrument];
+        return notes.indexOf(note);
+    }
+    return -1;
+}
+
+// Function to get the next note in the scale based on the current note and interval
+function getNextNoteInScale(currentNote, interval) {
+    const allNotes = staticData.allNotes;
+    const currentIndex = allNotes.indexOf(currentNote);
+    const nextIndex = (currentIndex + interval) % allNotes.length;
+    return allNotes[nextIndex];
+}
+
+// Event listener for when user selects an instrument
+function handleInstrumentChange(event) {
+  selectedInstrument = event.target.value;
+  populateFretboard();
+}
+
+// Event listener for when user selects a tuning
+function handleTuningChange(event) {
+  selectedTuning = event.target.value;
+  populateFretboard();
+}
+
+// Event listener for when user selects a key
+function handleKeyChange(event) {
+  selectedKey = event.target.value;
+}
+
+// Event listener for when user selects a scale type
+function handleScaleTypeChange(event) {
+  selectedScaleType = event.target.value;
+  populateFretboard();
+}
+
+// Initialize the fretboard when the page loads
+populateFretboard()
--- fretwizard-svelte/components/fretboard.svelte
+++ fretwizard-svelte/components/fretboard.svelte
@@ -2,26 +2,28 @@
 <script>
     import { onMount } from "svelte";
     import { Router } from 'svelte-routing';
+    import { populateFretboard, calculateDots } from "./fretboard.js"
+
     export let selectedInstrument;
     export let selectedTuning;
     export let selectedKey;
     export let selectedScale;
+    export let NUM_FRETS = 18;
+
 
     let fretboardNotes = [];
-
-    // Define the tuningOptions object based on your JSON data
     let tuningOptions = {};
 
     // Function to update the fretboardNotes array
     function updateFretboardNotes() {
-        // Clear the existing fretboardNotes array
-        fretboardNotes = [];
+    // Clear the existing fretboardNotes array
+    fretboardNotes = [];
 
         // Loop through the frets and strings to populate the fretboardNotes array
         for (let stringIndex = 0; stringIndex < NUM_STRINGS; stringIndex++) {
             for (let fretIndex = 0; fretIndex <= NUM_FRETS; fretIndex++) {
                 // Calculate the note at this position based on the instrument and tuning
-                const noteAtPosition = mapNoteAtPosition(stringIndex, fretIndex);
+                const noteAtPosition = populateFretboard(selectedInstrument, selectedTuning, stringIndex, fretIndex);
 
                 // Push the note to the fretboardNotes array
                 fretboardNotes.push({ string: stringIndex, fret: fretIndex, note: noteAtPosition });
@@ -52,6 +54,12 @@
         }
     }
 
+    setContext('fretboardNotes', fretboardNotes);
+
+    onMount(updateFretboardNotes);
+    $: {
+        updateFretboardNotes()
+    }
 </script>
 
 <div class="fretboard">
@@ -69,100 +77,42 @@
                 <!-- Create rows for strings and display notes -->
                 {#each Array(NUM_STRINGS) as stringIndex}
                 <tr>
-                <th>{selectedTuning[stringIndex]}</th>
-                {#each Array(NUM_FRETS + 1) as fretIndex}
-                    <td>{getNoteForPosition(stringIndex, fretIndex)}</td>
-                {/each}
+                    <th>{selectedTuning[stringIndex]}</th>
+                    {#each Array(NUM_FRETS + 1) as fretIndex}
+                        <td>{getNoteForPosition(stringIndex, fretIndex)}</td>
+                    {/each}
                 </tr>
                 {/each}          
         </tbody>
     </table>
 </div>
 
-
-
-
+<!-- Fretboard.svelte CSS -->
 <style>
-    /* Add your CSS styles here */
+.fretboard {
+    position: relative;
+}
+
+/* Style the fretboard table */
+.fretboard-table {
+    border-collapse: collapse;
+}
+
+/* Style the table cells */
+.fretboard-table td {
+    width: 40px; /* Adjust the cell width as needed */
+    height: 20px; /* Adjust the cell height as needed */
+    border: 1px solid #000; /* Add cell borders */
+    text-align: center; /* Center-align the content */
+    vertical-align: middle; /* Vertically center-align the content */
+}
+
+/* Style the dot */
+.dot {
+    width: 10px; /* Adjust the dot size as needed */
+    height: 10px; /* Adjust the dot size as needed */
+    background-color: red; /* Set the dot color */
+    border-radius: 50%; /* Make the dot circular */
+    margin: auto; /* Center-align the dot within the cell */
+}
 </style>
-  
-  
-    // Call the updateFretboardNotes function when the component mounts and whenever selectedInstrument or selectedTuning changes
-    onMount(updateFretboardNotes);
-    $: {
-      updateFretboardNotes();
-    }
-  
-    function calculateDots(key, scale) {
-      // Calculate dot positions based on the selected key and scale
-      // You'll need logic to determine which frets and strings correspond to the selected notes in the scale
-      // Return an array of objects with x and y coordinates for each dot
-      return [
-        { string: 0, fret: 0 }, // Example dot position
-        // Add more dot positions as needed
-      ];
-    }
-  
-    setContext('fretboardNotes', fretboardNotes);
-  </script>
-  
-  <div class="fretboard">
-    <!-- Create a table to represent the fretboard -->
-    <table class="fretboard-table">
-      <thead>
-        <!-- Create a row for fret numbers -->
-        <tr>
-          {#each Array(NUM_FRETS + 1).fill().map((_, index) => (
-            <th>{index}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {#each Array(NUM_STRINGS).fill().map((_, stringIndex) => (
-          <tr>
-            {#each Array(NUM_FRETS + 1).fill().map((_, fretIndex) => (
-              <!-- Add logic to determine if a dot should be displayed -->
-              {#if shouldDisplayDot(stringIndex, fretIndex)}
-                <td class="dot-cell">
-                  <!-- You can style the dot with CSS -->
-                  <div class="dot"></div>
-                </td>
-              {:else}
-                <td></td>
-              {/if}
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-  
-  <!-- Fretboard.svelte CSS -->
-  <style>
-    .fretboard {
-      position: relative;
-    }
-  
-    /* Style the fretboard table */
-    .fretboard-table {
-      border-collapse: collapse;
-    }
-  
-    /* Style the table cells */
-    .fretboard-table td {
-      width: 40px; /* Adjust the cell width as needed */
-      height: 20px; /* Adjust the cell height as needed */
-      border: 1px solid #000; /* Add cell borders */
-      text-align: center; /* Center-align the content */
-      vertical-align: middle; /* Vertically center-align the content */
-    }
-  
-    /* Style the dot */
-    .dot {
-      width: 10px; /* Adjust the dot size as needed */
-      height: 10px; /* Adjust the dot size as needed */
-      background-color: red; /* Set the dot color */
-      border-radius: 50%; /* Make the dot circular */
-      margin: auto; /* Center-align the dot within the cell */
-    }
-  </style>  
